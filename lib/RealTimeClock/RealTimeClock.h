3c5d5965e3a08a7d6c0086395b6eb1d2f06f6845
mike <m.stewart177@btinternet.com>
2023-10-10 15:20:07 +0100

    Encapsulate Real Time Clock

---
+++ lib/RealTimeClock/RealTimeClock.h
@@ -0,0 +1,134 @@
+//
+// Created by mike on 10/10/23.
+//
+#pragma once
+
+#include <Arduino.h>
+#include <RV3028.h>
+
+namespace RealTimeClock
+{
+    struct DateTime
+    {
+    public:
+        DateTime() = default;
+
+        DateTime(byte year, byte month, byte date, byte hour, byte minute, byte second) :
+                Year(year),
+                Month(month),
+                Date(date),
+                Hour(hour),
+                Minute(minute),
+                Second(second)
+        {}
+
+        byte Year;
+        byte Month;
+        byte Date;
+        byte Hour;
+        byte Minute;
+        byte Second;
+
+        bool operator>(const DateTime &other)
+        {
+            if (this->Year > other.Year)
+            {
+                return true;
+            }
+
+            if (this->Year == other.Year && this->Month > other.Month)
+            {
+                return true;
+            }
+
+            if (this->Year == other.Year && this->Month == other.Month && this->Date > other.Date)
+            {
+                return true;
+            }
+
+            if (this->Year == other.Year && this->Month == other.Month && this->Date == other.Date &&
+                this->Hour > other.Hour)
+            {
+                return true;
+            }
+
+            if (this->Year == other.Year && this->Month == other.Month && this->Date == other.Date &&
+                this->Hour == other.Hour && this->Minute > other.Minute)
+            {
+                return true;
+            }
+
+            if (this->Year == other.Year && this->Month == other.Month && this->Date == other.Date &&
+                this->Hour == other.Hour && this->Minute == other.Minute && this->Second > other.Second)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        bool operator==(DateTime const& other)
+        {
+            if (this->Year != other.Year)
+            {
+                return false;
+            }
+
+            if (this->Month != other.Month)
+            {
+                return false;
+            }
+
+            if (this->Date != other.Date)
+            {
+                return false;
+            }
+
+            if (this->Hour != other.Hour)
+            {
+                return false;
+            }
+
+            if (this->Minute != other.Minute)
+            {
+                return false;
+            }
+
+            if (this->Second != other.Second)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    };
+
+    class RealTimeClock
+    {
+    public:
+        RealTimeClock();
+
+    public:
+        void Initialise();
+
+        void CheckDaylightSavingsTime();
+
+        DateTime &GetDateTime();
+
+        time_t GetUnixTime();
+
+        String TimeStamp();
+
+    private:
+        void UpdateToInternetTime();
+
+    private:
+        RV3028 m_RTC;
+        DateTime m_dateTime;
+        time_t m_unixTime;
+        bool m_daylightSavingsTime;
+        byte m_dstOffsetMinutes;
+        DateTime m_dstChange;
+    };
+
+} // RealTimeClock
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,22 @@
 #include <Arduino.h>
 
 #include <Adafruit_NeoPixel.h>
-#include <RV3028.h>
 
 #include <WiFiUtils.h>
 #include <LedClock.h>
+#include "../lib/RealTimeClock/RealTimeClock.h"
 
 // Set LED_BUILTIN if it is not defined by Arduino framework
 #define LED_BUILTIN     13
 #define NEOPIXEL_PIN    16
 #define NUM_PIXELS      60
 
-#define RV3028_I2C_ADDR 0x52
-
 #define HOLD 200
 
 //#define DEBUG
 
 LedClock::LedClock *ledClock;
-RV3028* RTC;
+RealTimeClock::RealTimeClock* RTC;
 
 void setup()
 {
@@ -42,63 +40,35 @@ void setup()
     ledClock = new LedClock::LedClock(config, colours);
     ledClock->InitialiseDisplay();
 
-    JSONVar json = GetJson("worldtimeapi.org", 80, "/api/ip");
-
-    Serial.println(json);
-    Serial.println();
-
-    uint32_t internet_unix_time = (unsigned long)json["unixtime"];
-    String utc_date_time = json["utc_datetime"];
-    String local_date_time = json["datetime"];
-
-    Serial.print("Unix time: ");
-    Serial.println( (unsigned long)json["unixtime"]);
-    Serial.print("Date time (local):");
-    Serial.println(local_date_time);
-    Serial.print("Date time (UTC):");
-    Serial.println(utc_date_time);
-
     Serial.println("       RTC Functions      ");
     Serial.println("--------------------------");
 
     // Start RTC
-    RTC = new RV3028();
-    RTC->Begin();
-    uint32_t rtc_unix_time = RTC->Get_UNIX_Time();
-    if (abs((int)(rtc_unix_time - internet_unix_time)) > 2)
-    {
-        byte year = local_date_time.substring(2, 4).toInt();
-        byte month = local_date_time.substring(5, 7).toInt();
-        byte day = local_date_time.substring(8, 10).toInt();
-        byte hour = local_date_time.substring(11,13).toInt();
-        byte minute = local_date_time.substring(14, 16).toInt();
-        byte second = local_date_time.substring(17, 19).toInt();
-
-        Serial.print(String(year) + "-" + String(month) + "-" + String(day) + " ");
-        Serial.println(String(hour) + ":" + String(minute) + ":" + String(second));
-
-        RTC->Set_Time(second, minute, hour, day, month, year);
-        RTC->Update_Time_Stamp();
-        RTC->Set_UNIX_Time(internet_unix_time);
-    }
+    RTC = new RealTimeClock::RealTimeClock();
+    RTC->Initialise();
 }
 
 void loop()
 {
-// write your code here
-    RTC->Update_Time_Stamp();
 //    Serial.println("Hello. world!");
-    Serial.println(RTC->Time_Stamp);
-    Serial.println(RTC->Get_UNIX_Time());
+    Serial.println(RTC->GetUnixTime());
 
+    RealTimeClock::DateTime dateTime = RTC->GetDateTime();
 #ifdef DEBUG
-    int minutes = RTC->Get_Second();
-    int hours = RTC->Get_Minute() % 12;
+    int minutes = dateTime.Second;
+    int hours = dateTime.Minute % 12;
 #else
-    int minutes = RTC->Get_Minute();
-    int hours = RTC->Get_Hour();
+    int minutes = dateTime.Minute;
+    int hours = dateTime.Hour;
 #endif
 
+    if (dateTime.Hour == 2 && dateTime.Minute == 0 && dateTime.Second)
+    {
+        Serial.println("Check Daylights Savings Time.");
+        RTC->CheckDaylightSavingsTime();
+    }
+
+    Serial.println(RTC->TimeStamp());
     ledClock->UpdateTime(hours, minutes);
     delay(HOLD);
 }
